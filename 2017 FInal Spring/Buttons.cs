b3f7c03f97700c22be2ffa9bf6c9593c2a4767e0
Unknown <g.belt97@gmail.com>
2017-04-17 10:46:44 -0600

    Add, Sub, Mult & Divide.

--- 2017 FInal Spring/Buttons.cs
+++ 2017 FInal Spring/Buttons.cs
@@ -3,62 +3,47 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-//To get starting, myCharacter and Answer as a number. my best options are to click as to add and Ac or enter as return. Like I've done with FunctionsWithReturns and Math Operators including ElseClause.
-//How do I equality? I found my way to do this Answer.Add("number");. 
-//As for return, example = 10 + 10; which is equal to 20 as a return but how can I do that?
-// To do this if(myNumber == "1") { i.Add("1");} but it shows error. This is int for numbers but unsure to do the same thing as like ElseClause ( if (myCharacter == "+") and other thing ). A different loop. 
 
-public class Buttons : MonoBehaviour {
+public class Buttons : MonoBehaviour
+{
+    public InputField Sir;
+    public InputField super;
+    public InputField dogs;
+    public InputField sugar;
+    public InputField happy;
+    public InputField great;
+    public InputField down;
+    public InputField up;
+    public InputField cheers;
+    public InputField hello;
+    public InputField sweet;
+    public InputField good;
 
-    public string myCharacters;
-    public List<string> myAnswer;
-    public float i;
-    public InputField myInput;
-    
-    
-
-
-    // Use this for initialization
-    public void Function (string myCharacters)
+    public void AddButton()
     {
-
-        if (myCharacters == "+")
-        {
-            myAnswer.Add("+");
-        }
-
-        if (myCharacters == "/")
-        {
-           myAnswer.Add("/");
-        }
-
-        if (myCharacters == "*")
-        {
-            myAnswer.Add("*");
-        }
-        
-        if (myCharacters == "-")
-        {
-            myAnswer.Add("-");
-            
-        }
-
-        if (myCharacters == " ")
-        {
-            float i = float.Parse(myCharacters);
-            
-            return i;
-        }
-
-        
+        float sum;
+        sum = float.Parse(Sir.text) + float.Parse(super.text);
+        dogs.text = sum.ToString();
     }
 
-    public void ButtonClick()
+    public void AddCharacter()
     {
-        foreach (string myCharacters in myAnswer)
-        {
-            print(myAnswer);
-        }
+        float sub;
+        sub = float.Parse(sugar.text) - float.Parse(happy.text);
+        great.text = sub.ToString();
+    }
+
+    public void AddBalls()
+    {
+        float multiple;
+        multiple = float.Parse(cheers.text) * float.Parse(down.text);
+        up.text = multiple.ToString();
+    }
+
+    public void AddDays()
+    {
+        float divide;
+        divide = float.Parse(hello.text) / float.Parse(sweet.text);
+        good.text = divide.ToString();
     }
 }
-
